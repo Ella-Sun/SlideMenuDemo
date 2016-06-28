ca4789155cd751fdacf7f64c60d39bd0cdfdc6ad
sunhong <sunhong@ucfgroup.com>
2016-06-28 15:59:51 +0800

    微调。

--- YZFilterMenu/Filter/Controller/FilterBaseViewController.m
+++ YZFilterMenu/Filter/Controller/FilterBaseViewController.m
@@ -3,7 +3,7 @@
 //  YZFilterMenu
 //
 //  Created by IOS-Sun on 16/5/19.
-//  Copyright © 2016年 mark. All rights reserved.
+//  Copyright © 2016年 Sunhong. All rights reserved.
 //
 
 #import "FilterBaseViewController.h"
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 //     Do any additional setup after loading the view.
+    
 //    UIBarButtonItem *backBarItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_back_red"] style:UIBarButtonItemStylePlain target:self action:@selector(navBackBarAction:)];
 //    self.navigationItem.leftBarButtonItem = backBarItem;
     self.view.backgroundColor = [UIColor colorWithWhite:0.952 alpha:1.000];
--- YZFilterMenu/Filter/Model/FilterSelectModel.m
+++ YZFilterMenu/Filter/Model/FilterSelectModel.m
@@ -3,7 +3,7 @@
 //  JDSelectDemo
 //
 //  Created by IOS-Sun on 16/5/19.
-//  Copyright © 2016年 mark. All rights reserved.
+//  Copyright © 2016年 Sunhong. All rights reserved.
 //
 
 #import "FilterSelectModel.h"
